3faed05b4189e10bb350dc4a7d74b11a32eb9f59
Son Pham-Ba <son.phamba@epfl.ch>
2026-05-27 23:56:41 +0200

    feat: remove empty drawer and toolbar components

--- frontend/src/components/AppToolbar.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <q-toolbar class="q-pa-sm"> </q-toolbar>
-</template>
-
-<script setup lang="ts"></script>
--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -2,8 +2,6 @@
   <q-layout view="hHh lpR fFf">
     <q-header bordered class="bg-white text-grey-10">
       <q-toolbar>
-        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
-
         <q-toolbar-title>
           <span class="text-primary text-bold on-right">{{ t('main.brand') }}</span>
         </q-toolbar-title>
@@ -12,13 +10,7 @@
           <img src="EPFL.svg" height="20px" class="on-left" />
         </a>
       </q-toolbar>
-      <q-separator />
-      <AppToolbar />
     </q-header>
-
-    <q-drawer v-model="leftDrawerOpen" bordered>
-      <left-drawer class="q-mt-md" />
-    </q-drawer>
 
     <q-page-container>
       <router-view />
@@ -27,14 +19,6 @@
 </template>
 
 <script setup lang="ts">
-import AppToolbar from 'src/components/AppToolbar.vue'
-import LeftDrawer from 'src/components/LeftDrawer.vue'
 
 const { t } = useI18n()
-
-const leftDrawerOpen = ref(false)
-
-function toggleLeftDrawer() {
-  leftDrawerOpen.value = !leftDrawerOpen.value
-}
 </script>
